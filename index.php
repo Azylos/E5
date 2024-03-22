eb6b241d73658740e9d32fd8aa7ee117db65cae2
Florian <florian.hoarau385@gmail.com>
2024-03-22 11:19:42 +0400

    Commencement de la page index de vente de jeux.

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RunGame - Votre boutique favorite de vente de jeux</title>
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
 <body>
+<!-- Parallax background image -->
+
+    <div class="sample-header">
+        <div class="sample-header-section">
+            <h1>Scroll down to see the parallax effect</h1>
+            <h2>Background landcape scrolls with its own depth </h2>
+        </div>
+    </div>
+
+<!-- Parallax background image -->
+
+
+
     
+<script src="./js/index.js"></script>
 </body>
 </html>
