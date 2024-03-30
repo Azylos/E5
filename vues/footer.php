6b1af5cc410fb3f70faf34855a07af2c3944fb19
Toma BARRET <tomabarret@outlook.fr>
2024-03-30 16:23:27 +0400

    fix

--- vues/footer.php
+++ vues/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
       <div class="col-lg-12">
-        <p>Copyright © Runest. Tous droits réservés. &nbsp;&nbsp;</p>
+        <p>Copyright © Rungame. Tous droits réservés. &nbsp;&nbsp;</p>
       </div>
     </div>
 </footer>
