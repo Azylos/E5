fa630504153765fd8c8bdbf41449ef50cd669067
Toma BARRET <tomabarret@outlook.fr>
2024-04-01 11:54:52 +0400

    add page for form newgame

---
+++ backend/newGame.php
@@ -0,0 +1,84 @@
+<?php require_once "lib/vérifSession.php" ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Rungame - Jeux vidéos</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="../assets/css/fontawesome.css">
+    <link rel="stylesheet" href="../assets/css/templatemo-lugx-gaming.css">
+    <link rel="stylesheet" href="../assets/css/owl.css">
+    <link rel="stylesheet" href="../assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 589 lugx gaming
+
+https://templatemo.com/tm-589-lugx-gaming
+
+-->
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <?php
+    require_once("vues/navbar.php");
+  ?>
+  <!-- ***** Header Area End ***** -->
+
+  <!-- <div class="main-banner">
+      <div class="container">
+      <div class="row">
+          <div class="col-lg-6 align-self-center">
+          <div class="caption header-text">
+              <h6>Bienvenue chez RUNGAME</h6>
+              <h2>MEILLEUR SITE DE JEU VIDEO !</h2>
+              <p>à mettre quelque chose</p>
+              <div class="search-input">
+
+              </div>
+          </div>
+          </div>
+      </div>
+      </div>
+  </div> -->
+
+  <?php
+    require_once("../vues/footer.php")
+  ?>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="../assets/js/isotope.min.js"></script>
+  <script src="../assets/js/owl-carousel.js"></script>
+  <script src="../assets/js/counter.js"></script>
+  <script src="../assets/js/custom.js"></script>
+
+  </body>
+</html>
