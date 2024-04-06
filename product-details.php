2c8c997d29cc180c5bb6a118394baf6cbe9bbcae
Toma BARRET <tomabarret@outlook.fr>
2024-04-06 17:20:46 +0400

    mise en page

--- product-details.php
+++ product-details.php
@@ -161,7 +161,6 @@ $connexion = null;
                   <li class="nav-item" role="presentation">
                     <button class="nav-link active" id="description-tab" data-bs-toggle="tab" data-bs-target="#description" type="button" role="tab" aria-controls="description" aria-selected="true">Description</button>
                   </li>
-
                 </ul>
               </div>              
               <div class="tab-content" id="myTabContent">
@@ -169,11 +168,7 @@ $connexion = null;
                   <?php
                     echo "<p>".$description."</p>"
                   ?>
-                  <!-- <p>You can search for more templates on Google Search using keywords such as "templatemo digital marketing", "templatemo one-page", "templatemo gallery", etc. Please tell your friends about our website. If you need a variety of HTML templates, you may visit Tooplate and Too CSS websites.</p>
-                  <br>
-                  <p>Coloring book air plant shabby chic, crucifix normcore raclette cred swag artisan activated charcoal. PBR&B fanny pack pok pok gentrify truffaut kitsch helvetica jean shorts edison bulb poutine next level humblebrag la croix adaptogen. Hashtag poke literally locavore, beard marfa kogi bruh artisan succulents seitan tonx waistcoat chambray taxidermy. Same cred meggings 3 wolf moon lomo irony cray hell of bitters asymmetrical gluten-free art party raw denim chillwave tousled try-hard succulents street art.</p> -->
                 </div>
-
               </div>
             </div>
           </div>
